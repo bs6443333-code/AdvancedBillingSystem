877d91c8c36936f7cb63ff5790f460f47a11898f
bs6443333-code <bs6443333@gmail.com>
2026-05-04 08:56:41 +0530

    main file

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+	
+		
+	class Program
+	{
+		static void Main()
+		{
+			B b = new Usage();
+
+			decimal amt = b.Calc(100);
+			amt = Discount.Apply(amt);
+
+			Invoice i = new Invoice();
+			i.Amount = amt;
+
+			i.FinalizeInv();
+			i.Pay();
+
+			Console.WriteLine("Amount: " + i.Amount);
+			Console.WriteLine("State: " + i.State);
+		}
+	}
